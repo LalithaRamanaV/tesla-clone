7f5890b106baefab2c935b5ea78576552ba54488
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-13 21:08:56 +0530

    build: updated header

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from "styled-components"
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Container = styled.div`
 min-height:60px;
@@ -10,18 +11,34 @@ position:fixed;
 display:flex;
 align-items:center;
 padding:0 20px;
+justify-content:space-between;
 ` 
 const Menu = styled.div`
  display:flex;
  align-items:center;
  flex:1;
  justify-content:center;
- p{
+ a{
   font-weight:600;
   text-transform:uppercase;
   padding:0 10px;
   flex-wrap:nowrap;
  }
+ @media(max-width:768px){
+    display:none;
+ }
+`
+const RightMenu = styled.div`
+display:flex;
+align-items:center;
+a{
+    font-weight:600;
+    text-transform:uppercase;
+    margin-right:10px;
+}
+`
+const CustomMenu = styled(MenuIcon)`
+cursor:pointer;
 `
 
 const Header = () => {
@@ -31,11 +48,16 @@ const Header = () => {
         <img src="images/logo.svg" alt=""/>
        </a>
        <Menu>
-        <p><a href="/">Model S</a></p>
-        <p><a href="/">Model X</a></p>
-        <p><a href="/">Model Y</a></p>
-        <p><a href="/">Model 3</a></p>
+        <a href="/">Model S</a>
+        <a href="/">Model X</a>
+        <a href="/">Model Y</a>
+        <a href="/">Model 3</a>
        </Menu>
+       <RightMenu>
+        <a href="/">Shop</a>
+        <a href="/">Tesla Account</a>
+       </RightMenu>
+       <CustomMenu/>
     </Container>
   )
 }
