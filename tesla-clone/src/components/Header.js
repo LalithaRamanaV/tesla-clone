49d9c1a618108a1117f154378a36276e0c6728b3
Lalitha Vadavalli <sailalithavadavalli98@gmail.com>
2022-06-13 20:37:14 +0530

    build: update header.js

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+min-height:60px;
+position:fixed;
+display:flex;
+align-items:center;
+padding:0 20px;
+` 
+const Menu = styled.div`
+
+`
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Container>
+       <a href="/">
+        <img src="images/logo.svg" alt=""/>
+       </a>
+       <Menu>
+        <p><a href="/">Model S</a></p>
+        <p><a href="/">Model X</a></p>
+        <p><a href="/">Model Y</a></p>
+        <p><a href="/">Model 3</a></p>
+       </Menu>
+    </Container>
   )
 }
 
